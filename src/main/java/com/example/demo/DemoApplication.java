63406fe5e6ac67a3f807e9adf9ec862169bea88d
Xianqi Zhang <zhangxianqica@gmail.com>
2021-01-27 16:22:30 -0500

    after 1st lesson

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,9 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
@@ -14,8 +12,11 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+	@PostMapping("/hello")
+	public String hello(@RequestParam(value = "name", required = false) String name,
+						@RequestBody(required = false) String body) {
+		return String.format("Hello %s %s!", name, body);
 	}
 }
+
+//@Request
