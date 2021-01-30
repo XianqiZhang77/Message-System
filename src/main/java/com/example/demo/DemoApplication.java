7949b63d3a2e5c7dd2172615efd12a07765f33ad
Xianqi Zhang <zhangxianqica@gmail.com>
2021-01-30 09:53:01 -0500

    @PathVariable

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,11 +12,22 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	/*
 	@PostMapping("/hello")
 	public String hello(@RequestParam(value = "name", required = false) String name,
 						@RequestBody(required = false) String body) {
 		return String.format("Hello %s %s!", name, body);
 	}
+	 */
+
+
+	@GetMapping(path="/hello/{name}/{age}")
+	public String hello(@PathVariable("name") String name, @PathVariable("age") String age) {
+
+		String msg = String.format("%s is %s years old", name, age);
+		return msg;
+	}
+
 }
 
 //@Request
