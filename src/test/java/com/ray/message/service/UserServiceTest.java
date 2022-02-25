cb3035287383612c54e18f200497cdca8140d9f6
Jianjin Fan <jianjinf@amazon.com>
2022-02-25 17:33:58 -0500

    add hello world test

--- src/test/java/com/ray/message/service/UserServiceTest.java
+++ src/test/java/com/ray/message/service/UserServiceTest.java
@@ -1,5 +1,6 @@
 package com.ray.message.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 import com.ray.message.dao.UserDao;
@@ -31,5 +32,10 @@ public class UserServiceTest {
         user.setPassword("hi");
 
         this.userService.register(user);
+    }
+
+    @Test
+    public void testHelloWorld_happyCase() throws Exception {
+        assertEquals(3, 1+2);
     }
 }
